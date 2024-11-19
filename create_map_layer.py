45fd75c1f1ff65078c722c3392a818a910f0abe5
danilaloko <67982929+danilaloko@users.noreply.github.com>
2024-11-19 17:39:48 +0300

    Delete create_map_layer.py

--- create_map_layer.py
+++
@@ -1,8 +0,0 @@
-import folium
-
-# Задайте начальные координаты и масштаб
-lat, lon = 55.7558, 37.6176  # Москва, например
-m = folium.Map(location=[lat, lon], zoom_start=20)
-
-# Сохраните карту в HTML
-m.save('map.html')
